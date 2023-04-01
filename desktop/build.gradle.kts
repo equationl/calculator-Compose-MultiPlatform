70029021e9fce079b30771b5eb598cfdc6b07823
equationl <equationl@qq.com>
2023-04-01 18:22:19 +0800

    修复 distributions 包闪退问题

--- desktop/build.gradle.kts
+++ desktop/build.gradle.kts
@@ -29,6 +29,7 @@ compose.desktop {
     application {
         mainClass = "MainKt"
         nativeDistributions {
+            modules("java.sql")
             targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb)
             packageName = "隐云计算器"
             packageVersion = "1.0.0"
